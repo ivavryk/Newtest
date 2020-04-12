1e5b3d569720d687b530c376942cf0df88738c30
ivavryk <igor08@meta.ua>
2020-04-12 23:06:27 +0300

    Add files via upload

--- Class2.cs
+++ Class2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Allure.Commons;
 using NUnit.Allure.Attributes;
 using NUnit.Allure.Core;
 using NUnit.Framework;
@@ -19,21 +20,24 @@ namespace Newtest
         public class Class2
         {
             [Test]
+            [AllureSeverity(SeverityLevel.minor)]
             public static void Test4()
             {
                 Assert.AreEqual(Math.Max(3, 5), 5, "Incorrect max function.");
             }
 
             [Test]
+            [AllureSeverity(SeverityLevel.critical)]
             public static void Test5()
             {
                 Assert.AreEqual(Math.Min(3, 5), 4, "Incorrect min function.");
             }
 
             [Test]
+            [AllureSeverity(SeverityLevel.blocker)]
             public static void Test6()
             {
-                Assert.AreEqual(Math.Abs(-3), 3, "Incorrect abs function.");
+                Assert.AreEqual(Math.Abs(-3), 2, "Incorrect abs function.");
             }
         }
     }
